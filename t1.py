e6c60def2a433b6c893ea059a93a8280eced2cec
Mike <velez.michael.n@gmail.com>
2022-05-19 14:38:07 -0600

    more practice

---
+++ t1.py
@@ -0,0 +1,8 @@
+new_old = "New"
+first_name = "John"
+last_name = "Smith"
+age = "20"
+
+print (f'Patient Status: {new_old}')
+print (f'Patient Name: {first_name} {last_name}')
+print (f'Age: {age}')
---
+++ t2.py
@@ -0,0 +1,5 @@
+fist_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+birth_year = input('hello ' + fist_name + " " + last_name + ' what year were you born? ')
+age = 2022 - int(birth_year)
+print("you should be about " + age + "years old.")
